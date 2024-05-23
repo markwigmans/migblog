d7b05a904c1c9ee7d8838f4f8203c88f7ad615e5
buildingsoftwareblocks <mark.wigmans@gmail.com>
2024-05-23 13:21:49 +0200

    initial version

---
+++ src/main/java/com/btb/migblog/MigblogApplication.java
@@ -0,0 +1,13 @@
+package com.btb.migblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MigblogApplication.class, args);
+    }
+
+}
